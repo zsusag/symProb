647e2adb7edd4f49ec134c06df11be2031adcf61
Zachary J. Susag <zjs@zacharysusag.net>
2023-02-02 16:46:39 -0500

    Regenerate logs after changes to tool

--- test/psi_examples/benchmark.sh
+++ test/psi_examples/benchmark.sh
@@ -7,10 +7,10 @@ no_prob_suffix="-no_prob"
 for example in *.pp
 do
 		echo "Running $example..."
-		/usr/bin/time -p symprog "$example" > "${example%.*}"$log_suffix 2>&1
+		/usr/bin/time -p symProb "$example" > "${example%.*}"$log_suffix 2>&1
 		echo "Finished running $example..."
 
 		echo "Running $example with no probabilities..."
-		/usr/bin/time -p symprog -n "$example" > "${example%.*}"$no_prob_suffix$log_suffix 2>&1
+		/usr/bin/time -p symProb -n "$example" > "${example%.*}"$no_prob_suffix$log_suffix 2>&1
 		echo "Finished running $example with no probabilities..."
 done
